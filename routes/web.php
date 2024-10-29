a2eb8e679f7e5e21d3fcef6f8ae2d6fb3005f125
Ivanrdz95 <al04515485@tecmilenio.mx>
2024-10-28 22:19:30 -0600

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
+
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StatusController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Ruta para las órdenes
+Route::resource('orders', OrderController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Ruta para los usuarios
+Route::resource('users', UserController::class);
+
+// Ruta para los estados
+Route::resource('statuses', StatusController::class);
